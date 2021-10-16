83f30a3f5fd80ff3689aad96ada811fffbc5ffeb
shaker09 <shakermahmud66@gmail.com>
2021-10-16 21:43:30 +0600

    auth-intergation

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBznIJNH8a-HLIPsCgqLneALrg-l6W1gKE",
+    authDomain: "simple-firebase-authenti-74689.firebaseapp.com",
+    projectId: "simple-firebase-authenti-74689",
+    storageBucket: "simple-firebase-authenti-74689.appspot.com",
+    messagingSenderId: "595130922225",
+    appId: "1:595130922225:web:770a89e0c3a7dc30992550"
+  };
+
+  export default firebaseConfig;
---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <h2>This is Order place</h2>
+            <h4>Make an order and give us payment through Bkash</h4>
+        </div>
+    );
+};
+
+export default PlaceOrder;
